44f39d199bf21dcb8e562952a39136faca244508
Doshik <stedasha28@mail.ru>
2024-08-14 22:02:22 +0300

    correct the error

---
+++ src/classes.py
@@ -0,0 +1,78 @@
+import random
+import sqlite3
+from src.constants import *
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.image = player_img
+        self.score = 0 
+        self.x = WIDTH // 2
+        self.y = HEIGHT - 60
+        self.speed = 12
+        self.bullets = []
+
+    def move(self, dx, dy):
+        self.x += dx
+        if self.x < 0:
+            self.x = 0
+        elif self.x > WIDTH - self.image.get_width():
+            self.x = WIDTH - self.image.get_width()
+
+    def draw(self):
+        win.blit(self.image, (self.x, self.y))
+        
+    def shoot(self):
+        bullet = Bullet(self.x + self.image.get_width() // 2, self.y)
+        self.bullets.append(bullet)
+
+class Enemy:
+    def __init__(self, speed):
+        self.image = enemy_img
+        self.x = random.randint(0, WIDTH - self.image.get_width())
+        self.y = random.randint(-100, -40)
+        self.speed = speed
+
+    def draw(self):
+        win.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        self.y += self.speed
+
+class Bullet:
+    def __init__(self, x, y):
+        self.image = bullet_img
+        self.x = x
+        self.y = y
+        self.speed = 10
+
+    def draw(self):
+        win.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        self.y -= self.speed
+
+
+class Database:
+    def __init__(self, db_name="scores.db"):
+        self.connection = sqlite3.connect(db_name)
+        self.create_table()
+
+    def create_table(self):
+        with self.connection:
+            self.connection.execute(
+                "CREATE TABLE IF NOT EXISTS scores (name TEXT, score INTEGER)"
+            )
+
+    def insert_score(self, player):
+        with self.connection:
+            self.connection.execute(
+                "INSERT INTO scores (name, score) VALUES (?, ?)",
+                (player.name, player.score)
+            )
+
+    def get_top_scores(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM scores ORDER BY score DESC LIMIT 5")
+        return cursor.fetchall()
---
+++ src/constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+bullet_img = pygame.Surface((10, 20))
+bullet_img.fill((0, 255, 0))
+player_img = pygame.transform.scale(pygame.image.load("images/playerr.png"), (60, 60))
+enemy_img = pygame.transform.scale(pygame.image.load("images/enemy.png"), (70, 70))
